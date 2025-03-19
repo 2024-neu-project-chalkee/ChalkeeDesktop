ac77f036f45f0af1a6ccdf032697bb65c094e4e3
maurik1830 <nagyemoric@gmail.com>
2025-03-19 23:56:45 +0100

    LoadTimeTables function mostly done

--- ChalkeeDesktopLib/AppUI.cs
+++ ChalkeeDesktopLib/AppUI.cs
@@ -145,6 +145,12 @@ public class AppUI(AuthService authService)
     {
         try
         {
+
+            List<TimeTable> dailySchedules= [];
+
+
+            
+
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("C:\\Users\\Móric2\\OneDrive\\Dokumentumok\\Neu12b\\ikt\\Aminisztrációs projekt (NYÁRON CSINÁLNI)\\ChalkeeConsole\\ChalkeeDesktop\\appsettings.json").Build();
 
@@ -152,22 +158,62 @@ public class AppUI(AuthService authService)
             await using var dataSource = NpgsqlDataSource.Create(connectionString!);
 
 
-            await using var command = dataSource.CreateCommand("SELECT name FROM subjects");
-            await using var reader = await command.ExecuteReaderAsync();
+            string queryString = "WITH user_role AS (SELECT role FROM users  WHERE id = @user_id), timetable_combined AS ( SELECT  tc.day,  tc.period,  s2.name AS subject, CONCAT(u2.first_name, ' ', u2.last_name) AS name, r2.name AS room, CASE  WHEN c2.id IS NULL THEN NULL  ELSE CONCAT(c2.number, '.', c2.letter)  END AS class, g2.name AS \"group\", gr2.name AS grouproom, cr2.name AS classroom, tc.status, c2.id AS class_id, g2.id AS group_id, u2.id AS teacher_id FROM timetable_changes tc LEFT JOIN classes c2 ON tc.class_id = c2.id LEFT JOIN groups g2 ON tc.group_id = g2.id LEFT JOIN users u2 ON tc.teacher_id = u2.id LEFT JOIN rooms r2 ON tc.room_id = r2.id LEFT JOIN subjects s2 ON tc.subject_id = s2.id LEFT JOIN rooms cr2 ON c2.classroom = cr2.id AND c2.id IS NOT NULL LEFT JOIN rooms gr2 ON g2.grouproom = gr2.id AND g2.id IS NOT NULL UNION ALL SELECT  t.day,  t.period,  s1.name AS subject, CONCAT(u1.first_name, ' ', u1.last_name) AS name, r1.name AS room, CASE  WHEN c1.id IS NULL THEN NULL  ELSE CONCAT(c1.number, '.', c1.letter)  END AS class, g1.name AS \"group\", gr1.name AS grouproom, cr1.name AS classroom, NULL AS status, c1.id AS class_id, g1.id AS group_id, u1.id AS teacher_id FROM timetables t LEFT JOIN classes c1 ON t.class_id = c1.id LEFT JOIN groups g1 ON t.group_id = g1.id LEFT JOIN users u1 ON t.teacher_id = u1.id LEFT JOIN rooms r1 ON t.room_id = r1.id LEFT JOIN subjects s1 ON t.subject_id = s1.id LEFT JOIN rooms cr1 ON c1.classroom = cr1.id AND c1.id IS NOT NULL LEFT JOIN rooms gr1 ON g1.grouproom = gr1.id AND g1.id IS NOT NULL WHERE NOT EXISTS ( SELECT 1 FROM timetable_changes tc  WHERE tc.day = t.day AND tc.period = t.period ))SELECT day, period, subject, name, room, class, \"group\", classroom, grouproom, status FROM timetable_combined WHERE ( (SELECT role FROM user_role) = 'Student'  AND ( group_id IN ( SELECT group_id  FROM user_groups  WHERE user_id = @user_id ) OR class_id IN ( SELECT id  FROM classes  WHERE id = ( SELECT class_id  FROM users  WHERE id = @user_id ) ) )) OR ( (SELECT role FROM user_role) IN ('Teacher', 'Principal')  AND ( teacher_id = @user_id ))";
+
+            await using var command = dataSource.CreateCommand(queryString);
+            command.Parameters.AddWithValue("user_id", CurrentUser!.ID);
+            await using var TimetableReader = await command.ExecuteReaderAsync();
 
 
 
 
 
 
-            while (await reader.ReadAsync())
+            while (await TimetableReader.ReadAsync())
             {
-                Console.WriteLine(reader.GetString(0));
+                dailySchedules.Add(
+                    new TimeTable(
+                        Convert.ToInt32(TimetableReader["day"]),
+                        Convert.ToInt32(TimetableReader["period"]),
+                        TimetableReader["subject"].ToString()!,
+                        TimetableReader["name"].ToString()!,
+                        TimetableReader["room"].ToString()!,
+                        TimetableReader["class"] is null ? "--" : TimetableReader["class"].ToString()!,
+                        TimetableReader["group"] is null ? "--" : TimetableReader["group"].ToString()!,
+                        TimetableReader["classroom"] is null ? "--" : TimetableReader["classroom"].ToString()!,
+                        TimetableReader["grouproom"] is null ? "--" : TimetableReader["grouproom"].ToString()!,
+                        TimetableReader["status"] is null ? "--" : TimetableReader["status"].ToString()!
+                        
+                        
+
+
+                        )
+
+
+                        );
+
+                
+
+
+
+                //Console.WriteLine(TimetableReader.GetString(0));
             }
+
+            dailySchedules = [..dailySchedules.OrderBy(x => x.Day).OrderBy(x => x.Period)];
+
+            foreach (var day in dailySchedules)
+            {
+                Console.WriteLine(day.ToString());
+            }
+
+
+
+
         }
         catch (Exception e)
         {
             Console.WriteLine("Oops, an unexpected error occured!");
+            Console.WriteLine(e.Message);
         }
     }
 
